9b058959fb2b04fafce877eace43ce22ab866838
directduck <famgui14@gmail.com>
2020-01-06 18:06:51 +0300

    few color changes

--- simple.py
+++ simple.py
@@ -9,14 +9,23 @@ class Rectangle:
         self.rect_id = rect_id
         self.height = height
 
-    def highlight(self, color):
-        graph.TKCanvas.itemconfig(self.rect_id, fill=color)
+    def highlight(self, graph, color1, color2=None):
+        if color2 is None: color2 = color1
+        graph.TKCanvas.itemconfig(self.rect_id, fill=color1, outline=color2)
         window.Refresh()
 
     def __gt__(self, other):
         return self.height > other.height
 
+
 class Sorter:
+
+    BLUE = '#64778d'
+    BLUE_OUTLINE = '#5c6d81'
+    MAGENTA = '#a26ba2'
+    MAGENTA_OUTLINE = '#8d5d8d'
+    GREEN = '#6ba970'
+    GREEN_OUTLINE = '#5e9362'
 
     def __init__(self, window, graph):
         self.window = window
@@ -28,6 +37,7 @@ class Sorter:
         self.startx = 0
 
         self.array = None
+        self.values = None
         self.array_access = 0
 
     def draw_rects(self, slider_value):
@@ -35,42 +45,59 @@ class Sorter:
         x2 = x1 + self.bar_width
         k = 0
         self.array = []
+        self.values = []
         for _ in range(slider_value):
             number = random.randint(1, self.graph.Size[1])
+            self.values.append(number)
             rectangle_id = self.graph.DrawRectangle(
                 (x1, number),
                 (x2, 0.0),
-                fill_color='Black')
+                fill_color=Sorter.BLUE,
+                line_color=Sorter.BLUE_OUTLINE)
             self.array.append(Rectangle(rectangle_id, number))
             x1 = x2 + 1
             x2 = x1 + self.bar_width
 
     def redraw(self):
         self.array_access += 1
-        self.window.FindElement('text').Update(f'Array access: {self.array_access}')
+        self.window.FindElement('text').Update(
+            f'Array access: {self.array_access}')
         self.graph.Erase()
 
         for i, elem in enumerate(self.array):
             x1 = self.startx + (i) * (self.bar_width + 1)
-            elem.rect_id = self.graph.DrawRectangle(
-                (x1, elem.height), 
-                (x1 + self.bar_width, 0.0), 
-                fill_color='Black')
+            if elem.height == self.values[i]:
+                elem.rect_id = self.graph.DrawRectangle(
+                    (x1, elem.height),
+                    (x1 + self.bar_width, 0.0),
+                    fill_color=Sorter.BLUE,
+                    line_color=Sorter.BLUE_OUTLINE)
+            else:
+                self.values[i] = elem.height
+                elem.rect_id = self.graph.DrawRectangle(
+                    (x1, elem.height),
+                    (x1 + self.bar_width, 0.0),
+                    fill_color=Sorter.MAGENTA,
+                    line_color=Sorter.MAGENTA_OUTLINE)
 
         window.Refresh()
         time.sleep(1 / self.timeout)
 
     def color_all_green(self):
         for elem in self.array:
-            elem.highlight('Green')
+            elem.highlight(self.graph, Sorter.BLUE, Sorter.BLUE_OUTLINE)
+
+        for elem in self.array:
+            elem.highlight(self.graph, Sorter.GREEN, Sorter.GREEN_OUTLINE)
             time.sleep(1 / self.timeout)
 
     def swap_rects(self, rect1, rect2):
         self.array_access += 1
-        self.window.FindElement('text').Update(f'Array access: {self.array_access}')
+        self.window.FindElement('text').Update(
+            f'Array access: {self.array_access}')
 
-        # rect1.highlight('Green')
-        # rect2.highlight('Green')
+        rect1.highlight(self.graph, Sorter.MAGENTA, Sorter.MAGENTA_OUTLINE)
+        rect2.highlight(self.graph, Sorter.MAGENTA, Sorter.MAGENTA_OUTLINE)
         time.sleep(1 / self.timeout)
 
         rect1_upper_left_x = self.graph.GetBoundingBox(rect1.rect_id)[0][0]
@@ -85,15 +112,16 @@ class Sorter:
         self.window.Refresh()
         time.sleep(1 / self.timeout)
 
-        # rect1.highlight('Black')
-        # rect2.highlight('Black')
+        rect1.highlight(self.graph, Sorter.BLUE, Sorter.BLUE_OUTLINE)
+        rect2.highlight(self.graph, Sorter.BLUE, Sorter.BLUE_OUTLINE)
 
     def bubble(self):
         for i in range(len(self.array) - 1):
             for j in range(len(self.array) - i - 1):
                 if self.array[j].height > self.array[j + 1].height:
                     self.swap_rects(self.array[j], self.array[j + 1])
-                    self.array[j], self.array[j + 1] = self.array[j + 1], self.array[j]
+                    self.array[j], self.array[j +
+                                              1] = self.array[j + 1], self.array[j]
             self.window.Refresh()
 
     def partition(self, start, end):
@@ -143,20 +171,12 @@ class Sorter:
 
             while i < len(lefthalf) and j < len(righthalf):
                 if lefthalf[i].height <= righthalf[j].height:
-                    # self.array[k].highlight('Green')
-                    # lefthalf[i].highlight('Green')
-                    time.sleep(1 / self.timeout)
-
                     self.array[k] = lefthalf[i]
                     arr[k_or] = lefthalf[i]
                     i += 1
 
                     self.redraw()
                 else:
-                    # self.array[k].highlight('Green')
-                    # righthalf[j].highlight('Green')
-                    time.sleep(1 / self.timeout)
-
                     self.array[k] = righthalf[j]
                     arr[k_or] = righthalf[j]
                     j += 1
@@ -166,10 +186,6 @@ class Sorter:
                 k_or += 1
 
             while i < len(lefthalf):
-                # self.array[k].highlight('Green')
-                # lefthalf[i].highlight('Green')
-                time.sleep(1 / self.timeout)
-
                 self.array[k] = lefthalf[i]
                 arr[k_or] = lefthalf[i]
                 i += 1
@@ -179,10 +195,6 @@ class Sorter:
                 self.redraw()
 
             while j < len(righthalf):
-                # self.array[k].highlight('Green')
-                # righthalf[j].highlight('Green')
-                time.sleep(1 / self.timeout)
-
                 self.array[k] = righthalf[j]
                 arr[k_or] = righthalf[j]
                 j += 1
@@ -194,80 +206,81 @@ class Sorter:
     def gnome(self):
         i, size = 1, len(self.array)
         while i < size:
-            if self.array[i - 1].height <= self.array[i].height: i += 1
+            if self.array[i - 1].height <= self.array[i].height:
+                i += 1
             else:
                 self.swap_rects(self.array[i - 1], self.array[i])
                 self.array[i - 1], self.array[i] = self.array[i], self.array[i - 1]
-                if i > 1: i -= 1
+                if i > 1:
+                    i -= 1
 
-    def countingSort(self, exp1): 
-        n = len(self.array) 
-        output = [0] * (n) 
-        count = [0] * (10) 
-      
-        for i in range(0, n): 
-            index = int(self.array[i].height/exp1) 
-            count[ (index)%10 ] += 1
-      
-        for i in range(1,10): 
-            count[i] += count[i-1] 
-      
-        i = n-1
-        while i >= 0: 
-            index = int(self.array[i].height/exp1) 
-            output[ count[ (index)%10 ] - 1] = self.array[i]
-            count[ (index)%10 ] -= 1
+    def countingSort(self, exp1):
+        n = len(self.array)
+        output = [0] * (n)
+        count = [0] * (10)
+
+        for i in range(0, n):
+            index = int(self.array[i].height / exp1)
+            count[(index) % 10] += 1
+
+        for i in range(1, 10):
+            count[i] += count[i - 1]
+
+        i = n - 1
+        while i >= 0:
+            index = int(self.array[i].height / exp1)
+            output[count[(index) % 10] - 1] = self.array[i]
+            count[(index) % 10] -= 1
             i -= 1
 
         i = 0
-        for i in range(0,len(self.array)):
+        for i in range(0, len(self.array)):
             self.array[i] = output[i]
             self.redraw()
 
-    def radix(self): 
-        max1 = max(self.array).height 
+    def radix(self):
+        max1 = max(self.array).height
         exp = 1
-        while self.array != sorted(self.array): 
-            self.countingSort(exp) 
+        while self.array != sorted(self.array):
+            self.countingSort(exp)
             exp *= 10
-  
 
 
 WIDTH = 850
 HEIGHT = 400
 
-graph = sg.Graph((WIDTH, HEIGHT), 
-                 (0, 0), 
+graph = sg.Graph((WIDTH, HEIGHT),
+                 (0, 0),
                  (WIDTH, HEIGHT),
-                 background_color='#ffffff', float_values=True)
+                 background_color='#fff', float_values=True)
 
 layout = [
-          [sg.Button('Start'),
-           sg.Button('Merge'),
-           sg.Button('Quicksort'),
-           sg.Button('Bubble'),
-           sg.Button('Gnome'),
-           sg.Button('Radix')],
+    [sg.Button('Start'),
+     sg.Button('Merge'),
+     sg.Button('Quicksort'),
+     sg.Button('Bubble'),
+     sg.Button('Gnome'),
+     sg.Button('Radix')],
 
-           [sg.Slider(range=(10, 200), orientation='horizontal', key='slider',
-                      change_submits=True, disable_number_display=True),
-            sg.Text('Array access: 0       ', key='text')],
-           
-           [graph],
+    [sg.Slider(range=(10, 200), orientation='horizontal', key='slider',
+               change_submits=True, disable_number_display=True),
+     sg.Text('Array access: 0       ', key='text')],
+
+    [graph],
 ]
 
 
 def draw_screen(sorter, slider_value):
     sorter.graph.Erase()
 
-    sorter.bar_width = int(WIDTH / (slider_value * 2))
-    freespace = WIDTH - (sorter.bar_width + 1) * slider_value
-    sorter.startx = int(freespace / 2)
+    sorter.bar_width = int(sorter.graph.Size[0] / (slider_value * 2))
+    free_space = sorter.graph.Size[0] - (sorter.bar_width + 1) * slider_value
+    sorter.startx = int(free_space / 2)
 
     sorter.draw_rects(slider_value)
 
 
-window = sg.Window('Window Title', layout, finalize=True)
+window = sg.Window('Sorting Visualizer', layout, finalize=True)
 
 sorter = Sorter(window, graph)
 
