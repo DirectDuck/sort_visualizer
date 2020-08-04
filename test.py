ac21c0911c6c65c43c7e929dda25d455718b8d44
dduck <famgui14@gmail.com>
2020-08-04 16:07:31 +0300

    fixed execution in test.py

--- test.py
+++ test.py
@@ -31,13 +31,14 @@ class TestBubbleSort(algs.Sort):
                     self.array[j], self.array[j + 1] = self.array[j + 1], self.array[j]
 
 
-gui = gui.GUI()
+if __name__ == '__main__':
+    gui = gui.GUI()
 
-gui.add_sort('Bubble', algs.BubbleSort)
-gui.add_sort('Merge', algs.MergeSort)
-gui.add_sort('Radix', algs.RadixSort)
-gui.add_sort('Gnome', algs.GnomeSort)
-gui.add_sort('Quick', algs.QuickSort)
-gui.add_sort('Test', TestBubbleSort)
+    gui.add_sort('Bubble', algs.BubbleSort)
+    gui.add_sort('Merge', algs.MergeSort)
+    gui.add_sort('Radix', algs.RadixSort)
+    gui.add_sort('Gnome', algs.GnomeSort)
+    gui.add_sort('Quick', algs.QuickSort)
+    gui.add_sort('Test', TestBubbleSort)
 
-gui.run()
+    gui.run()
